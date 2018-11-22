66021cabc51d205d4c58585e7af0988c73641695
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 22:08:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,65 @@
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+
+def using_push(array, string)
+  array.push(string)
+  array
+end
+
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+new_neighborhood = "Brooklyn Heights"
+def using_unshift(array, string)
+  array.unshift(string)
+  array
+end
+
+great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+def using_pop(array)
+  array.pop
+end
+
+chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+def pop_with_args(array)
+  array.pop(2)
+end
+
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+def using_shift(array)
+  array.shift
+end
+
+ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+def shift_with_args(array)
+  array.shift(2)
+end
+
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+def using_concat(array, new)
+  array.concat(new)
+  array
+end
+
+list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+def using_insert(array, new_sect)
+  array.insert(4, new_sect)
+end
+
+captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+def using_uniq(array)
+  array.uniq
+end
+
+private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+def using_flatten(array)
+  array.flatten
+end
+
+instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+def using_delete(array, string)
+  array.delete(string)
+end
+
+famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
